429c4339b17063243330ca3718419e45865a1c3d
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-11-28 21:33:53 +0300

    called the functions

--- js/script.js
+++ js/script.js
@@ -5,30 +5,33 @@ document.getElementById("alert2").style.display = "none";
 //declare arrays
 
 var daysOfTheWeek=[
-"Sunday"
-"Monday"
-"Wednesday"
-"Thursday"
-"Friday"
-"Saturday"
+"Sunday",
+"Monday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
 ];
 
 var maleNames=[
-"Kwasi"
-"Kwadwo"
-"Kwabena"
-"Kwaku"
-"Yaw"
-"Kofi"
-"Kwame"
+"Kwasi",
+"Kwadwo",
+"Kwabena",
+"Kwaku",
+"Yaw",
+"Kofi",
+"Kwame",
 ];
 
 var femaleNames=[
-"Akosua"
-"Adwoa"
-"Abenaa"
-"Akua"
-"Yaa"
-"Afua"
-"Ama"
+"Akosua",
+"Adwoa",
+"Abenaa",
+"Akua",
+"Yaa",
+"Afua",
+"Ama",
 ];
+
+var month = parseInt(document.getElementById("month").value);
+var year =parseInt(document.getElementById(year).value);
