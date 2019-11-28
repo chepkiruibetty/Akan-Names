e6ba2266089e371bf1f667d8049c1d8fc38f4f62
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-11-28 22:05:27 +0300

    modified functions

--- js/script.js
+++ js/script.js
@@ -33,5 +33,13 @@ var femaleNames=[
 "Ama",
 ];
 
+//calling functions
+
 var month = parseInt(document.getElementById("month").value);
-var year =parseInt(document.getElementById(year).value);
+var year =parseInt(document.getElementById("year").value);
+var day =parseInt(document.getElementById("day").value);
+var name =parseInt(document.get.ElementById("name"));
+var dateOfBirth =newDate(year +"/"+ month + "/" +day);
+var output=parseInt(document.getElementById(output).value);
+var male=parseInt(document.getElementById("male"));
+var female=parseInt(document.getElementById("female"));
