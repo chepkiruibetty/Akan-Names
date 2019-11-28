9f178d93c2d1e6336b66d7752d1796eb9cca7dbf
Tony Kihara <tonykiharatonkin@gmail.com>
2019-11-28 15:55:32 +0300

    declared the arrays

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//hide the alert button
+document.getElementById("alert1").style.display = "none";
+document.getElementById("alert2").style.display = "none";
+
+//declare arrays
+
+var daysOfTheWeek=[
+"Sunday"
+"Monday"
+"Wednesday"
+"Thursday"
+"Friday"
+"Saturday"
+];
+
+var maleNames=[
+"Kwasi"
+"Kwadwo"
+"Kwabena"
+"Kwaku"
+"Yaw"
+"Kofi"
+"Kwame"
+];
+
+var femaleNames=[
+"Akosua"
+"Adwoa"
+"Abenaa"
+"Akua"
+"Yaa"
+"Afua"
+"Ama"
+];
