4355a4353e30b2069edb234085ebc3d96c273517
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-11-29 09:43:44 +0300

    declared the functioning arrays

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//declaring arrays
+
+var daysOfWeek=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+var MaleNames=[
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+];
+
+var femaleNames=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaw",
+    "Afua",
+    "Ama",
+];
