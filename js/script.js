5122ec29742e718166da96b1eabb3c11dc9b2a5b
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-11-29 11:55:09 +0300

    added the functions

--- js/script.js
+++ js/script.js
@@ -29,3 +29,12 @@ var femaleNames=[
     "Afua",
     "Ama",
 ];
+
+//calling functions
+var month =parseInt(document.getElementById("month").value);
+var year=parseInt(document.getElementById("year").value);
+var day=parseInt(document.getElementById("day").value);
+var name=parseInt(document.getElementById("name").value);
+var output=parseInt(document.getElementById("output").value);
+var male=parseInt(document.getElementById("male").value);
+var female=parseInt(document.getElementById("female").value);
